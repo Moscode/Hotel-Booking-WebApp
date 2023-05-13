0bb4f799d7a2a5e31e44dc3b65969dfbc537e946
Odunosh Moses <Odunoshomoses@gmail.com>
2023-05-13 20:25:43 +0000

    client only

--- booking-server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-const dbConfig = require('./db')
-
-const port = process.env.port || 5001
-
-app.listen(port, () => console.log(`Server running on port ${port}`))
