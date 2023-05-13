26ed9b2ff64e0e097e0b8aec955028e697005510
Odunosh Moses <Odunoshomoses@gmail.com>
2023-05-13 20:29:31 +0000

    backend without node module

---
+++ booking-server/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+let { MONGO_URL } = process.env
+
+mongoose.connect(MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+let connection = mongoose.connection
+
+connection.on('error', ()=>{
+    console.log("Mongo DB connection fail")
+})
+
+connection.on('connected', ()=>{
+    console.log("Mongo DB connected successfully")
+})
+
+module.exports = mongoose
