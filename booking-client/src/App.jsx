49e94b44ca28a0e9fb9c6caf14fe9480da3048ea
Odunosh Moses <34396268+Moscode@users.noreply.github.com>
2023-05-15 13:56:02 +0000

    routing implemented

--- booking-client/src/App.jsx
+++ booking-client/src/App.jsx
@@ -1,20 +1,10 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootsra'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
-       <Navbar variant="dark" bg="dark"> 
-        <Container>
-          <Navbar.Brand>RideHive</Navbar.Brand>
-          <Nav>
-            <Nav.Link href="register">Register</Nav.Link>
-            <Nav.Link href="login">Login</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+       <NavBar />
     </>
   )
 }
